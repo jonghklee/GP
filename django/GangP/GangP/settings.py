7d508d328d8b67071ec64e2ffac7caba634c2712
jonghklee <jonghklee@naver.com>
2021-06-26 11:00:07 +0900

    'all'

--- django/GangP/GangP/settings.py
+++ django/GangP/GangP/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'GangP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['GangP/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
