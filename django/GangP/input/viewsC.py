488daa4db6bcf953652b0230722a132d487bfb33
jonghklee <jonghklee@naver.com>
2021-06-26 10:48:48 +0900

    'l'

--- django/GangP/input/viewsC.py
+++ django/GangP/input/viewsC.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.utils.translation import check_for_language
 from .models import SoftwareSpec, ChoosenSpec
 # Create your views here.
 #종혁
@@ -8,6 +7,13 @@ def page1(request):
     soft = SoftwareSpec.objects.all()
     choosen = ChoosenSpec.objects.all()
     return render(request, 'page1.html', {'soft':soft, 'shoosen':choosen})
+
+
+    format
+
+
+
+    213
 
 
 # name과 옵션선택이 분리되어있어서 문제발생 - request.POST[''] 방식으로 받아오면됨.
