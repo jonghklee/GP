6177072158355aedddfbcabd9b543baa4b9f1f23
jonghklee <jonghklee@naver.com>
2021-06-27 10:32:34 +0900

    a

--- django/GangP/input/admin.py
+++ django/GangP/input/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SoftwareSpec, ChoosenSpec
+from .models import SoftwareSpec, ChoosenSpec, othermatters
 # Register your models here.
 
 admin.site.register(SoftwareSpec)
 admin.site.register(ChoosenSpec)
+admin.site.register(othermatters)
