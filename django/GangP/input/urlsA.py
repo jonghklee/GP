189f149092481030744cea7fa221a9dbd2b4ca98
geniehyeon31 <hgenie3113@gmail.com>
2021-06-28 16:43:01 +0900

    create?!

--- django/GangP/input/urlsA.py
+++ django/GangP/input/urlsA.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from input import viewsA
+from .viewsA import *
+
 
 urlpatterns = [
     path('page2', viewsA.page2 , name='page2'),
+    path('create/',create, name="create")
+
 ]
--- django/GangP/input/viewsA.py
+++ django/GangP/input/viewsA.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import othermatters
 # Create your views here.
 #현진
 
 def page2(request):
-    return render(request, 'page2.html')
+    blogs = othermatters.objects.all()
+    return render(request, 'page2.html', {'blogs':blogs})
+
+def create(request):
+    o_m__ = othermatters()
+    manufacturer = request.POST['제조사']
+    laptopweight1, laptopweight2 = request.POST['무게']
+    screensize = request.POST['화면크기']
+    operating = request.POST['운영체제']
+    o_m__.save()
+    return redirect('detail', o_m__.id)
+
+
