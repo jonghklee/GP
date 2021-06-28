6e012926ff401bd324d98076f77b9c29a68fb952
jonghklee <jonghklee@naver.com>
2021-06-28 18:35:26 +0900

    viewA

--- django/GangP/input/urlsA.py
+++ django/GangP/input/urlsA.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from input import viewsA
-from .viewsA import *
 
 
 urlpatterns = [
     path('page2', viewsA.page2 , name='page2'),
-    path('create/',create, name="create")
+    path('create/', viewsA.create, name="create")
 
 ]
--- django/GangP/input/viewsA.py
+++ django/GangP/input/viewsA.py
@@ -8,12 +8,20 @@ def page2(request):
     return render(request, 'page2.html', {'blogs':blogs})
 
 def create(request):
-    o_m__ = othermatters()
-    manufacturer = request.POST['제조사']
+    matter1 = othermatters()
+    a = request.POST
+
+    asasdf = ['asdf', 'asdfs']
+    
+    matter1.manufacturer = request.POST['제조사']
     laptopweight1, laptopweight2 = request.POST['무게']
     screensize = request.POST['화면크기']
     operating = request.POST['운영체제']
-    o_m__.save()
-    return redirect('detail', o_m__.id)
+    matter1.save()
+    return 
+
+    {'a' : a}
+
+
 
 
