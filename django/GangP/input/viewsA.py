83816ca70cbbac975bab965a38bd740e9b9daaf4
jonghklee <jonghklee@naver.com>
2021-06-29 20:00:49 +0900

    heddit

--- django/GangP/input/viewsA.py
+++ django/GangP/input/viewsA.py
@@ -8,10 +8,12 @@ def page2(request):
 
 def create(request):
     matter1 = othermatters()
-    matter1.manufacturer = request.POST['제조사']
-    matter1.laptopweight1 = request.POST['무게']
-    matter1.screensize = request.POST['화면크기']
-    matter1.operating = request.POST['운영체제']
+    matterlist = dict(request.POST.lists())
+    del matterlist['csrfmiddlewaretoken']
+    matter1.manufacturer = matterlist['제조사']
+    matter1.laptopweight1 = matterlist['무게']
+    matter1.screensize = matterlist['화면크기']
+    matter1.operating = matterlist['운영체제']
     matter1.save()
     return render(request, 'practice.html', {'matter1': matter1})
 
