b0a2ec28fac014296d2cffec1b826479b7207940
jonghklee <jonghklee@naver.com>
2021-07-01 06:33:04 +0900

    delnewhtml

--- django/GangP/input/urlsB.py
+++ django/GangP/input/urlsB.py
@@ -2,6 +2,6 @@ from django.urls import path
 from input import viewsB
 
 urlpatterns = [
+    path('page2', viewsB.page2 , name='page2'),
     path('page1_5', viewsB.page1_5, name='page1_5'),
-    path('new', viewsB.new, name='new'),
 ]
--- django/GangP/input/viewsA.py
+++ django/GangP/input/viewsA.py
@@ -3,8 +3,6 @@ from .models import othermatters
 # Create your views here.
 #현진
 
-def page2(request):
-    return render(request, 'page2.html')
 
 def create(request):
     matter1 = othermatters()
