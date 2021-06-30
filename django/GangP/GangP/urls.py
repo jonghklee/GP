35a8200c9192aeb89d9b9c13f8326b6d0b9c5683
jonghklee <jonghklee@naver.com>
2021-06-30 15:54:01 +0900

    loginout

--- django/GangP/GangP/urls.py
+++ django/GangP/GangP/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('A/', include('input.urlsA')),
     path('B/', include('input.urlsB')), 
     path('', include('input.urlsC')),
+    path('account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
